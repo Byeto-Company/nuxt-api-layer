829c306b0a5bb033b6f14b86e255fdf28bc35406
marzban-dev <marzban.dev@gmail.com>
2025-08-20 14:38:53 +0330

    Updated

---
+++ app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+    apiLayer: {
+        name: "Hello from Nuxt layer",
+    },
+});
+
+declare module "@nuxt/schema" {
+    interface AppConfigInput {
+        apiLayer?: {
+            /** Project name */
+            name?: string;
+        };
+    }
+}
