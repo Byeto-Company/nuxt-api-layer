fddce179acaaeab831630a0c539a4ddc8d7dea90
marzban-dev <marzban.dev@gmail.com>
2025-08-20 20:25:06 +0330

    Updated

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath } from "node:url";
 
 export default defineNuxtConfig({
-    extends: ["github:Byeto-Company/nuxt-auth-layer", ".."],
+    extends: ["github:Byeto-Company/nuxt-utils-layer", "github:Byeto-Company/nuxt-auth-layer", ".."],
     modules: ["@nuxt/eslint"],
     eslint: {
         config: {
