2bcc08cf51ad8a48f05e5805b20866d9d5ce1c11
marzban-dev <marzban.dev@gmail.com>
2025-11-29 11:24:25 +0330

    docs: Update playground

---
+++ .playground/app.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+    <AuthLayerWrapper check-on-server-prefetch>
+        <NuxtPage />
+    </AuthLayerWrapper>
+</template>
--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -1,8 +1,46 @@
 import { fileURLToPath } from "node:url";
 
 export default defineNuxtConfig({
-    extends: ["github:Byeto-Company/nuxt-utils-layer", "github:Byeto-Company/nuxt-auth-layer", ".."],
+    extends: ["@byeto/nuxt-utils-layer", "@byeto/nuxt-auth-layer", ".."],
     modules: ["@nuxt/eslint"],
+    authModule: {
+        internalPage: true,
+        otpCount: 4,
+        endpoints: {
+            develop_token: {
+                name: "",
+                path: "/user/develop_token",
+            },
+            logout: {
+                name: "",
+                path: "/user/logout",
+            },
+            otp: {
+                name: "",
+                path: "/user/send_otp",
+            },
+            profile: {
+                name: "",
+                path: "/user/profile",
+            },
+            refresh: {
+                name: "",
+                path: "/user/token/refresh",
+            },
+            signin: {
+                name: "",
+                path: "/user/token",
+            },
+            update: {
+                name: "",
+                path: "/user/profile",
+            },
+            verify: {
+                name: "",
+                path: "/user/verify",
+            },
+        },
+    },
     eslint: {
         config: {
             // Use the generated ESLint config for lint root project as well
